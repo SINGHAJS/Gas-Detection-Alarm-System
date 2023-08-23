2457c8a2dc76f8584cd797bc25ddb9eefd46be79
AJIT SINGH <Ajit_Singh_1@outlook.com>
2023-08-23 15:52:51 +1200

    imports the dual color led functions to update the color state based on the gas level

--- src/lcd.py
+++ src/lcd.py
@@ -1,4 +1,5 @@
 from rpi_lcd import LCD
+from dual_color_led import set_safe_alert_color, set_danger_alert_color
 
 lcd = LCD() # Create LCD instance
 
@@ -8,8 +9,12 @@ def update_gas_alarm_status(gas_value):
     # Display relevant message based on the gas level 
     if gas_value > 45 :
         lcd.text('WARNING HIGH GAS', 1)
+        set_danger_alert_color(True)
+        set_safe_alert_color(False)
     else:
-        lcd.text('SAFE GAS LEVEL', 1) 
+        lcd.text('SAFE GAS LEVEL', 1)
+        set_safe_alert_color(True) 
+        set_danger_alert_color(False)
 
 
 lcd.clear() # Clear LCD screen
