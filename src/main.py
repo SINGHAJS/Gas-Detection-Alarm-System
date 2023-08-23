4b50733e0a18804752b08724d3ffc36db134cab4
AJIT SINGH <Ajit_Singh_1@outlook.com>
2023-08-23 23:03:01 +1200

    adds multi-threading to lcd.py and main.py

--- src/main.py
+++ src/main.py
@@ -7,3 +7,6 @@ dual_color_led_thread = threading.Thread(target=start_dual_color_led)
 
 gas_sensor_thread.start()
 dual_color_led_thread.start()
+
+gas_sensor_thread.join()
+dual_color_led_thread.join()
